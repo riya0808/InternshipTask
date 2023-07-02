afed1c8f753c400a62c5f533481d88a61fd4db80
riya0808 <124344063+riya0808@users.noreply.github.com>
2023-07-02 10:42:53 +0530

    Add files via upload

---
+++ next.js
@@ -0,0 +1,18 @@
+const candidates = [
+    {
+        name: 'Riya Gupta',
+        jobTitle: 'Software Engineer',
+        location: 'New Delhi'
+    },
+    {
+        name: 'Jiya Rajput',
+        jobTitle: 'Marketing Manager',
+        location: 'Banglore'
+    },
+    {
+        name: 'Harshit',
+        jobTitle: 'Web Developer',
+        location: 'Chandigadh'
+    },
+    // Add more candidate objects as needed
+];
